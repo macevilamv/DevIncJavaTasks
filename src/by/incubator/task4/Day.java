d38dad763c1c813b22836981d8c2450adbb77b57
macevilamv <maksim.matsevila@gmail.com>
2022-06-27 09:06:25 +0300

    remade method

--- src/by/incubator/task4/Day.java
+++ src/by/incubator/task4/Day.java
@@ -1,40 +1,11 @@
 package by.incubator.task4;
 
 enum Day {
+
     MONDAY("MON",false), TUESDAY("TUE",false),
     WEDNESDAY("WED",false),    THURSDAY("THU",false),
     FRIDAY ("FRI", false), SATURDAY ("SAT", true),
     SUNDAY ("SUN", true);
-
-    public static void showDay (int i) {
-
-        switch (i) {
-            case 1 :
-                System.out.println(MONDAY + " (" + MONDAY.SHORT_NAME + ") " + MONDAY.MESSAGE);
-                break;
-            case 2 :
-                System.out.println(TUESDAY + " (" + TUESDAY.SHORT_NAME + ") " + TUESDAY.MESSAGE);
-                break;
-            case 3 :
-                System.out.println(WEDNESDAY + " (" + WEDNESDAY.SHORT_NAME + ") " + WEDNESDAY.MESSAGE);
-                break;
-            case 4 :
-                System.out.println(THURSDAY + " (" + THURSDAY.SHORT_NAME + ") " + THURSDAY.MESSAGE);
-                break;
-            case 5 :
-                System.out.println(FRIDAY + " (" + FRIDAY.SHORT_NAME + ") " + FRIDAY.MESSAGE);
-                break;
-            case 6 :
-                System.out.println(SATURDAY + " (" + SATURDAY.SHORT_NAME + ") " + SATURDAY.MESSAGE);
-                break;
-            case 7 :
-                System.out.println(SUNDAY + " (" + SUNDAY.SHORT_NAME + ") " + SUNDAY.MESSAGE);
-                break;
-            default:
-                System.out.println("Incorrect data");
-        }
-
-    }
 
     private final String SHORT_NAME;
     private final String MESSAGE;
@@ -50,6 +21,18 @@ enum Day {
             MESSAGE = "IS NOT A WEEKEND";
         }
 
+    }
+
+    public static void showInfo (int i) {
+        showDay (getDay (i));
+    }
+
+    private static void showDay (Day day) {
+        System.out.println (day.name() + ", " + day.SHORT_NAME + ", " + day.MESSAGE);
+    }
+
+    private static Day getDay (int dayNumber) {
+        return Day.values()[dayNumber - 1];
     }
 
 }
--- src/by/incubator/task4/DayOfTheWeek.java
+++ src/by/incubator/task4/DayOfTheWeek.java
@@ -3,9 +3,10 @@ package by.incubator.task4;
 import java.util.Scanner;
 
 public class DayOfTheWeek {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter day number: ");
-        Day.showDay(scanner.nextInt());
+        Day.showInfo(Integer.parseInt(scanner.nextLine()));
     }
 }
