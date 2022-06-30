7d89c93fd832f98a7630b50f18c303c1cf9d1085
macevilamv <maksim.matsevila@gmail.com>
2022-06-30 09:32:00 +0300

    fixed issues

--- src/by/incubator/task4/DayOfTheWeek.java
+++ src/by/incubator/task4/DayOfTheWeek.java
@@ -3,7 +3,6 @@ package by.incubator.task4;
 import java.util.Scanner;
 
 public class DayOfTheWeek {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter day number: ");
