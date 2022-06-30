5db6a7fa4c73f9c4ee290c7ed1ec78f03d98147e
macevilamv <maksim.matsevila@gmail.com>
2022-06-30 09:29:16 +0300

    fixed issues

--- autopark/src/by/incubator/autopark/Main.java
+++ autopark/src/by/incubator/autopark/Main.java
@@ -3,6 +3,18 @@ package by.incubator.autopark;
 public class Main {
     public static void main(String[] args) {
         VehicleType [] vehicleTypes = initArray();
+        increaseTaxValueForLastElement(vehicleTypes);
+        showInfo(vehicleTypes);
+    }
+
+    private static VehicleType[] initArray () {
+         return new VehicleType[] {new VehicleType("Bus", 1.2d),
+                new VehicleType("Car", 1d),
+                new VehicleType("Rink", 1.5d),
+                new VehicleType("Tractor", 1.2d)};
+    }
+
+    private static void showInfo(VehicleType [] vehicleTypes) {
         double max = vehicleTypes[0].getTaxCoefficient();
         double sum = 0.0d;
         double average = 0.0d;
@@ -13,7 +25,6 @@ public class Main {
             if (max < vehicleTypes[i].getTaxCoefficient()) {
                 max = vehicleTypes[i].getTaxCoefficient();
             }
-
             sum += vehicleTypes[i].getTaxCoefficient();
 
             if (i == vehicleTypes.length-1) {
@@ -21,18 +32,13 @@ public class Main {
             }
         }
 
-        vehicleTypes[vehicleTypes.length-1]
-                .setTaxCoefficient(vehicleTypes[vehicleTypes.length-1]
-                .getTaxCoefficient() + 1.3d);
-
         System.out.println(average);
         System.out.println(max);
     }
 
-    private static VehicleType[] initArray () {
-         return new VehicleType[] {new VehicleType("Bus", 1.2d),
-                new VehicleType("Car", 1d),
-                new VehicleType("Rink", 1.5d),
-                new VehicleType("Tractor", 1.2d)};
+    private static void increaseTaxValueForLastElement (VehicleType[] vehicleTypes) {
+        vehicleTypes[vehicleTypes.length-1]
+                .setTaxCoefficient(vehicleTypes[vehicleTypes.length-1]
+                .getTaxCoefficient() + 1.3d);
     }
 }
