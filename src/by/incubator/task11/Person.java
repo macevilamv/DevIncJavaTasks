a93321b324033712f9a1a157832f3adec738aa01
macevilamv <maksim.matsevila@gmail.com>
2022-07-04 16:48:23 +0300

    done task11

---
+++ src/by/incubator/task11/Person.java
@@ -0,0 +1,40 @@
+package by.incubator.task11;
+
+public class Person implements Comparable<Person>{
+    private String name;
+    private int age;
+
+    Person(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return Integer.compare(this.age, o.age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/by/incubator/task9/Voiceable.java
+++
@@ -1,7 +0,0 @@
-package by.incubator.task9;
-
-public interface Voiceable {
-    default void voice() {
-        System.out.println("voice");
-    }
-}
