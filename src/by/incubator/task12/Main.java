afa8fb668c89c9f6c51ff07b1267a163d430ca88
macevilamv <106157193+macevilamv@users.noreply.github.com>
2022-08-29 16:26:04 +0300

    Update Main.java

--- src/by/incubator/task12/Main.java
+++ src/by/incubator/task12/Main.java
@@ -3,7 +3,6 @@ package by.incubator.task12;
 public class Main {
     public static void main(String[] args) {
         String test = "((()()({})))";
-        String test2 = "()";
 
         System.out.println(BalanceOfBrackets.validateBrackets(test));
     }
